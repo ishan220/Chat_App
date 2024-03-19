560236f2d288019115e8df0f27ae73f3f12e9622
ishan220 <ishan@Ishans-MacBook-Air.local>
2024-03-20 03:28:23 +0530

    changed Db to supabase db

--- main.go
+++ main.go
@@ -10,23 +10,43 @@ import (
 	socket_pkg "github.com/ishan/Chat_App/pkg/websocket"
 )
 
-const connectionStr = "postgres://root:secret@localhost:5432/Realtime_Chat?sslmode=disable"
+// const connectionStr = "postgres://root:secret@localhost:5432/Realtime_Chat?sslmode=disable"
+const connectionStr = "user=postgres.cztvhelvpqmqvnbriapp password=ishurocks@1502 host=aws-0-ap-south-1.pooler.supabase.com port=5432 dbname=postgres"
 
-// func init() {
+// func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+// 	lc, ok := lambdacontext.FromContext(ctx)
+// 	if !ok {
+// 		return &events.APIGatewayProxyResponse{
+// 			StatusCode: 503,
+// 			Body:       "Something went wrong :(",
+// 		}, nil
+// 	}
 
+// 	cc := lc.ClientContext
+
+// 	return &events.APIGatewayProxyResponse{
+// 		StatusCode: 200,
+// 		Body:       "Hello, " + cc.Client.AppTitle,
+// 	}, nil
 // }
+
 func main() {
 
 	conn, err := sql.Open("postgres", connectionStr)
+
 	if err != nil {
 		fmt.Println("Error in connecting to Database")
 	}
+
 	store := db.NewStore(conn)
 	pool := socket_pkg.NewPool()
 
 	httpServer := httpserver.NewHttpServer(store, pool)
+
 	go pool.Start()
+	//go lambda.Start(handler) //aws lamda function to deploy on netlify
 	err1 := httpServer.Run()
+
 	if err1 != nil {
 		log.Fatal("Cannot start the server")
 	}
