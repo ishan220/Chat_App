783758a8cdd0123719b87a71f6d78e0ca17e7354
ishan220 <ishan@Ishans-MacBook-Air.local>
2024-04-11 13:26:29 +0530

    Allow surge domain

--- pkg/httpserver/server.go
+++ pkg/httpserver/server.go
@@ -30,7 +30,7 @@ func NewHttpServer(store *db.SQLStore, Pool *socket_pkg.Pool) *HttpServer {
 func (server *HttpServer) SetUpRoutes(Pool *socket_pkg.Pool) {
 	router := gin.Default()
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000", "http://localhost:3001", "http://localhost:3000/ws", "https://chat-on-go.netlify.app", "https://chat-app-delta-five.vercel.app/"}
+	config.AllowOrigins = []string{"http://localhost:3000", "http://localhost:3001", "http://localhost:3000/ws", "https://chat-on-go.netlify.app", "https://chat-app-delta-five.vercel.app/", "https://go-chat-app.surge.sh"}
 	router.Use(cors.New(config))
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"Status": "Success"})
